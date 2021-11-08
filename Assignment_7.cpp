712d8682364c6411ba14e842ebdde7f8976b52dc
unknown <devon.l.mckee@gmail.com>
2021-11-08 13:06:56 -0800

    Starting refactor for plantGrowth to Bezier patches, adding assignment 7

---
+++ Assignment_7.cpp
@@ -0,0 +1,206 @@
+// Assignment_7.cpp
+// (c) Devon McKee 2021
+
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include "Camera.h"
+#include "Draw.h"
+#include "GLXtras.h"
+#include "Misc.h"
+#include "Widgets.h"
+#include "VecMat.h"
+
+// display parameters
+int         winWidth = 800, winHeight = 600;
+Camera      camera(winWidth, winHeight, vec3(0, 0, 0), vec3(0, 0, -7));
+
+// shading
+GLuint      program = 0;
+int			textureName = 0, textureUnit = 0;
+const char *textureFilename = "textures/skyrim.tga";
+
+// interaction
+vec3        light(1.5f, 1.5f, 1);
+void       *picked = NULL;
+Mover       mover;
+time_t startTime = clock();
+
+// vertex shader (no op)
+const char *vShaderCode = R"(
+	#version 130
+	void main() {
+		gl_Position = vec4(0); // no-op
+	}
+)";
+
+// tessellation evaluation
+const char *teShaderCode = R"(
+	#version 400
+	layout (quads, equal_spacing, ccw) in;
+	uniform mat4 modelview, persp;
+	uniform sampler2D textureMap;
+	out vec3 point, normal;
+	out vec2 uv;
+	float heightFactor = 0.15;
+	float PI = 3.141592;
+	vec3 PtFromSphere(float u, float v) {
+		// u is longitude
+		// v is latitude (PI/2 = N. pole, 0 = equator, -PI/2 = S. pole)
+		float elevation = PI*v-PI/2;
+		float eFactor = cos(elevation);
+		float y = sin(elevation);
+		float angle = 2*PI*(1-u);
+		float x = eFactor*cos(angle), z = eFactor*sin(angle);
+		return vec3(x, y, z);
+	}
+	vec3 PtFromCyl(float u, float v) {
+		float elevation = PI*v-PI/2;
+		float eFactor = cos(elevation);
+		float y = sin(elevation);
+		float angle = 2*PI*(1-u);
+		float x = cos(angle), z = sin(angle);
+		return vec3(x, y, z);
+	}
+	void main() {
+		uv = gl_TessCoord.st;
+		//vec3 p = PtFromCyl(uv.s, uv.t);
+		vec3 p = vec3(uv.s, heightFactor*(texture(textureMap, uv).r), uv.t);
+		vec3 n = p;
+		point = (modelview*vec4(p, 1)).xyz;
+		normal = (modelview*vec4(n, 0)).xyz;
+		gl_Position = persp*vec4(point, 1);
+	}
+)";
+
+// pixel shader
+const char *pShaderCode = R"(
+	#version 130
+	in vec3 point, normal;
+	in vec2 uv;
+	uniform sampler2D textureMap;
+	uniform vec3 light;
+	void main() {
+		vec3 N = normalize(normal);             // surface normal
+		vec3 L = normalize(light-point);        // light vector
+		vec3 E = normalize(point);              // eye vertex
+		vec3 R = reflect(L, N);                 // highlight vector
+		float dif = max(0, dot(N, L));          // one-sided diffuse
+		float spec = pow(max(0, dot(E, R)), 50);
+		float ad = clamp(.15+dif, 0, 1);
+		vec3 texColor = texture(textureMap, uv).rgb;
+		gl_FragColor = vec4(ad*texColor+vec3(spec), 1);
+	}
+)";
+
+// display
+
+void Display() {
+	// background, blending, zbuffer
+	glClearColor(.6f, .6f, .6f, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glUseProgram(program);
+	// update matrices
+	SetUniform(program, "modelview", camera.modelview);
+	SetUniform(program, "persp", camera.persp);
+	//float dt = (float)(clock() - startTime) / CLOCKS_PER_SEC; // duration since start
+	//SetUniform(program, "time", (float)dt);
+	// transform light and send to pixel shader
+	vec4 hLight = camera.modelview*vec4(light, 1);
+	glUniform3fv(glGetUniformLocation(program, "light"), 1, (float *) &hLight);
+	// set texture
+	SetUniform(program, "textureMap", textureUnit);
+	glActiveTexture(GL_TEXTURE0+textureUnit);       // active texture corresponds with textureUnit
+	glBindTexture(GL_TEXTURE_2D, textureName);      // bind active texture to textureName
+	// tessellate patch
+	glPatchParameteri(GL_PATCH_VERTICES, 4);
+	float res = 100, outerLevels[] = {res, res, res, res}, innerLevels[] = {res, res};
+	glPatchParameterfv(GL_PATCH_DEFAULT_OUTER_LEVEL, outerLevels);
+	glPatchParameterfv(GL_PATCH_DEFAULT_INNER_LEVEL, innerLevels);
+	glDrawArrays(GL_PATCHES, 0, 4);
+	// light
+	glDisable(GL_DEPTH_TEST);
+	UseDrawShader(camera.fullview);
+	Disk(light, 12, vec3(1, 0, 0));
+	glFlush();
+}
+
+// mouse
+
+void MouseButton(GLFWwindow *w, int butn, int action, int mods) {
+	double x, y;
+	glfwGetCursorPos(w, &x, &y);
+	y = winHeight-y; // invert y for upward-increasing screen space
+	picked = NULL;
+	if (action == GLFW_RELEASE)
+		camera.MouseUp();
+	if (action == GLFW_PRESS) {
+		if (MouseOver(x, y, light, camera.fullview)) {
+			mover.Down(&light, (int) x, (int) y, camera.modelview, camera.persp);
+			picked = &mover;
+		}
+		else {
+			picked = &camera;
+			camera.MouseDown(x, y);
+		}
+	}
+}
+
+void MouseMove(GLFWwindow *w, double x, double y) {
+	if (glfwGetMouseButton(w, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		y = winHeight-y;
+		if (picked == &mover)
+			mover.Drag((int) x, (int) y, camera.modelview, camera.persp);
+		if (picked == &camera)
+			camera.MouseDrag((int) x, (int) y, Shift());
+	}
+}
+
+void MouseWheel(GLFWwindow *w, double ignore, double spin) {
+	camera.MouseWheel(spin > 0, Shift());
+}
+
+void Keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+// application
+
+void Resize(GLFWwindow *window, int width, int height) {
+	camera.Resize(winWidth = width, winHeight = height);
+	glViewport(0, 0, width, height);
+}
+
+int main(int ac, char **av) {
+	// init app window
+	if (!glfwInit())
+		return 1;
+	GLFWwindow *w = glfwCreateWindow(winWidth, winHeight, "Tess", NULL, NULL);
+	glfwSetWindowPos(w, 100, 100);
+	glfwMakeContextCurrent(w);
+	// init OpenGL, shader program, texture
+	gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+	program = LinkProgramViaCode(&vShaderCode, NULL, &teShaderCode, NULL, &pShaderCode);
+	textureName = LoadTexture(textureFilename, textureUnit);
+	// callbacks
+	glfwSetCursorPosCallback(w, MouseMove);
+	glfwSetMouseButtonCallback(w, MouseButton);
+	glfwSetScrollCallback(w, MouseWheel);
+	glfwSetKeyCallback(w, Keyboard);
+	glfwSetWindowSizeCallback(w, Resize);
+	// event loop
+	glfwSwapInterval(1);
+	while (!glfwWindowShouldClose(w)) {
+		Display();
+		glfwPollEvents();
+		glfwSwapBuffers(w);
+	}
+	glfwDestroyWindow(w);
+	glfwTerminate();
+}
--- plantGrowth.cpp
+++ plantGrowth.cpp
@@ -16,31 +16,94 @@ using std::min;
 using std::max;
 
 GLuint cubeBuffer = 0;
-GLuint program = 0;
+GLuint plantProgram = 1, cubeProgram = 2;
 
-int win_width = 800, win_height = 800;
+int win_width = 800, win_height = 800, res = 25;
 Camera camera((float)win_width / win_height, vec3(0, 0, 0), vec3(0, 0, -5));
 
 float cube_points[][3] = { {-1, -1, 1}, {1, -1, 1}, {1, -1, -1}, {-1, -1, -1}, {-1, 1, -1}, {1, 1, -1}, {1, 1, 1}, {-1, 1, 1} }; int cube_faces[][4] = { {0, 1, 2, 3}, {2, 3, 4, 5}, {4, 5, 6, 7}, {6, 7, 0, 1}, {0, 3, 4, 7}, {1, 2, 5, 6} };
-vector<vec3> plant_points;
+vec3 ctrlPts[] = {
+	vec3(0.2000f,  0.0000f, 2.70000f), vec3(0.2000f, -0.1120f, 2.70000f),
+	vec3(0.1120f, -0.2000f, 2.70000f), vec3(0.0000f, -0.2000f, 2.70000f),
+	vec3(1.3375f,  0.0000f, 2.53125f), vec3(1.3375f, -0.7490f, 2.53125f),
+	vec3(0.7490f, -1.3375f, 2.53125f), vec3(0.0000f, -1.3375f, 2.53125f),
+	vec3(1.4375f,  0.0000f, 2.53125f), vec3(1.4375f, -0.8050f, 2.53125f),
+	vec3(0.8050f, -1.4375f, 2.53125f), vec3(0.0000f, -1.4375f, 2.53125f),
+	vec3(1.5000f,  0.0000f, 2.40000f), vec3(1.5000f, -0.8400f, 2.40000f),
+	vec3(0.8400f, -1.5000f, 2.40000f), vec3(0.0000f, -1.5000f, 2.40000f)
+};
 
 const float GROWTH_INTERVAL = 0.05f;
 const float STALK_WIDTH = 0.05f;
 const int SAMPLE_NUM = 20;
 
+const char* plantVertShader = "void main() { gl_Position = vec4(0); }";
 
+const char* plantTessEvalShader = R"(
+	#version 400 core
+	layout (quads, equal_spacing, ccw) in;\
+	uniform vec3 ctrlPts[16];
+	uniform mat4 modelview;
+	uniform mat4 persp;
+	out vec3 point;
+	out vec3 normal;
+	out vec2 uv;
+	vec3 BezTangent(float t, vec3 b1, vec3 b2, vec3 b3, vec3 b4) {
+        float t2 = t*t;
+        return (-3*t2+6*t-3)*b1+(9*t2-12*t+3)*b2+(6*t-9*t2)*b3+3*t2*b4;
+    }
+    vec3 BezPoint(float t, vec3 b1, vec3 b2, vec3 b3, vec3 b4) {
+        float t2 = t*t, t3 = t*t2;
+        return (-t3+3*t2-3*t+1)*b1+(3*t3-6*t2+3*t)*b2+(3*t2-3*t3)*b3+t3*b4;
+    }
+	void main() {
+		vec3 spts[4], tpts[4];
+		uv = gl_TessCoord.st;
+		for (int i = 0; i < 4; i++) {
+			spts[i] = BezPoint(uv.s, ctrlPts[4*i], ctrlPts[4*i+1], ctrlPts[4*i+2], ctrlPts[4*i+3]);
+			tpts[i] = BezPoint(uv.t, ctrlPts[i], ctrlPts[i+4], ctrlPts[i+8], ctrlPts[i+12]);
+		}
+		vec3 p = BezPoint(uv.t, spts[0], spts[1], spts[2], spts[3]);
+		vec3 tTan = BezTangent(uv.t, spts[0], spts[1], spts[2], spts[3]);
+		vec3 sTan = BezTangent(uv.s, tpts[0], tpts[1], tpts[2], tpts[3]);
+        vec3 n = normalize(cross(sTan, tTan));
+		normal = (modelview*vec4(n, 0)).xyz;
+        point = (modelview*vec4(p, 1)).xyz;
+        gl_Position = persp*vec4(point, 1);
+	}
+)";
 
-const char* vertShader = R"(
+const char* plantFragShader = R"(
+	#version 130
+	in vec3 point;
+	in vec3 normal;
+	in vec2 uv;
+	out vec4 pColor;
+	uniform vec3 light = vec3(1, 1, 0);
+	void main() {
+		vec3 N = normalize(normal);
+		vec3 L = normalize(light - point);
+		vec3 E = normalize(point);
+		vec3 R = reflect(L, N);
+		float d = abs(dot(N, L));
+		float s = abs(dot(R, E));
+		float intensity = clamp(d+pow(s, 50), 0, 1);
+		vec3 color = vec3(1);
+		pColor = vec4(intensity*color, 1);
+	}
+)";
+
+const char* cubeVertShader = R"(
 	#version 130
 	in vec3 point;
 	uniform mat4 modelview;
 	uniform mat4 persp;
-	void main() {
+	void Main() {
 		gl_Position = persp * modelview * vec4(point, 1);
 	}
 )";
 
-const char* fragShader = R"(
+const char* cubeFragShader = R"(
 	#version 130
 	out vec4 pColor;
 	void main() {
@@ -108,20 +171,30 @@ void InitPlant() {
 }
 
 void Display() {
-	glClear(GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_DEPTH_BUFFER_BIT);
 	// Draw cube
-	glUseProgram(program);
+	glUseProgram(cubeProgram);
 	glBindBuffer(GL_ARRAY_BUFFER, cubeBuffer);
-	VertexAttribPointer(program, "point", 3, 0, (void*)0);
-	SetUniform(program, "persp", camera.persp);
-	SetUniform(program, "modelview", camera.modelview);
+	VertexAttribPointer(cubeProgram, "point", 3, 0, (void*)0);
+	SetUniform(cubeProgram, "persp", camera.persp);
+	SetUniform(cubeProgram, "modelview", camera.modelview);
 	for (int i = 0; i < 6; i++) {
 		glDrawElements(GL_LINE_LOOP, 4, GL_UNSIGNED_INT, cube_faces[i]);
 	}
 	// Draw plant
-
+	glUseProgram(plantProgram);
+	SetUniform(plantProgram, "modelview", camera.modelview);
+	SetUniform(plantProgram, "persp", camera.persp);
+	glPatchParameteri(GL_PATCH_VERTICES, 4);
+	float r = (float)res;
+	float outerLevels[] = { r, r, r, r }, innerLevels[] = { r, r };
+	glPatchParameterfv(GL_PATCH_DEFAULT_OUTER_LEVEL, outerLevels);
+	glPatchParameterfv(GL_PATCH_DEFAULT_INNER_LEVEL, innerLevels);
+	SetUniform3v(plantProgram, "ctrlPts", 16, (float*)ctrlPts);
+	glDrawArrays(GL_PATCHES, 0, 4);
 	glFlush();
 }
 
@@ -138,7 +211,9 @@ int main() {
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	PrintGLErrors();
-	if (!(program = LinkProgramViaCode(&vertShader, &fragShader)))
+	if (!(plantProgram = LinkProgramViaCode(&plantVertShader, NULL, &plantTessEvalShader, NULL, &plantFragShader)))
+		return 1;
+	if (!(cubeProgram = LinkProgramViaCode(&cubeVertShader, &cubeFragShader)))
 		return 1;
 	InitVertexBuffer();
 	glfwWindowHint(GLFW_SAMPLES, 4);
